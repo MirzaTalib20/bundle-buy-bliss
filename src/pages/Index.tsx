04249bf5b787bec4708293fa03dd1a44380b5547
Talib mirza <talib.equitec@gmail.com>
2025-04-26 17:45:01 +0530

    test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -20,7 +20,7 @@ import img3 from "@/assest/img/digital.jpg";
 import img4 from "@/assest/img/youtube.jpg";
 import img5 from "@/assest/img/premiumtshirt.png";
 import img6 from "@/assest/img/canva.png";
-import homeImg from "@/assest/img/home.png";
+import homeImg from "@/assest/img/Home.png";
 
 const BUNDLE_PRICE = 499;
 
